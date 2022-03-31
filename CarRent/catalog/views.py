d306cf2752dcb10edc49d0abd4ef9c80fc0f6fcc
andrey <andrai1998@mail.ru>
2022-03-31 08:07:58 +0200

    Add change view for html

--- CarRent/catalog/views.py
+++ CarRent/catalog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from .models import Car, Condition, Category, CarInstance, Status, Country, Client
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Главная страница сайта Аренда автомобилей!")
+    num_car = Car.objects.all().count()
+    num_instance = CarInstance.objects.all().count()
+
+    # Доступные автомобили
+    num_instance_available = CarInstance.objects.filter(status__exact=2).count()
+
+    return render(request, 'index.html',
+                  context={'num_car': num_car,
+                           'num_instance': num_instance,
+                           'num_instance_available': num_instance_available},
+                  )
